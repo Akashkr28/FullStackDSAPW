4ee1fd482ff22a953a6fcb422dd9722b00693797
Akashkr28 <email.akash2010@gmail.com>
2025-02-06 21:25:19 +0530

    Added friendsPair Solution

--- 7. Dynamic Programiing/fibonnaciNum.java
+++ 7. Dynamic Programiing/fibonnaciNum.java
@@ -18,3 +18,30 @@ public class fibonnaciNum {
 
 // Ques 1: Leetcode: 509. Fibonacci Number
 // Link: https://leetcode.com/problems/fibonacci-number/
+
+// Iterative Approach
+
+class Solution {
+    public static int[] dp = new int[100005];
+    public static int f(int i) {
+        if(i == 0 || i == 1) return i; // base case
+        if(dp[i] != -1) return dp[i]; // if value is not -1 we can directly return ans
+        //if the above case doesn't execute, it means sub problems has not been solved
+        return dp[i] = f(i-1) + f(i-2);
+    }
+
+    public static int f_bu(int n) {
+        dp = new int[100005];
+        dp[0] = 0;
+        dp[1] = 1;
+
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+        return dp[n];
+    }
+
+    public int fib(int n) {
+        return f_bu(n);
+    }
+}
