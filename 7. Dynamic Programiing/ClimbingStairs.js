359c0d869c0ec760622bc67b809ac9223ce4a260
Akashkr28 <email.akash2010@gmail.com>
2025-02-07 19:29:19 +0530

    Added climbing stairs solution

---
+++ 7. Dynamic Programiing/ClimbingStairs.js
@@ -0,0 +1,20 @@
+// LeetCode problem: 70. Climbing Stairs
+// link: https://leetcode.com/problems/climbing-stairs/
+
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    if (n === 1) return 1;
+    if (n === 2) return 2;
+    
+    let first = 1, second = 2;
+    for (let i = 3; i <= n; i++) {
+        let third = first + second;
+        first = second;
+        second = third;
+    }
+    
+    return second;
+};
